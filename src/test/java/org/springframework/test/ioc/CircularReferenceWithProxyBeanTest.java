7e531382642948fd32826d7389d879f77b717392
1835522428 <1835522428@qq.com>
2024-12-13 19:32:02 +0800

    三级缓存解决代理对象循环依赖

--- src/test/java/org/springframework/test/ioc/CircularReferenceWithProxyBeanTest.java
+++ src/test/java/org/springframework/test/ioc/CircularReferenceWithProxyBeanTest.java
@@ -12,7 +12,16 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
  * @date 2021/1/30
  */
 public class CircularReferenceWithProxyBeanTest {
+	/*
+		解决有代理对象时的循环依赖问题（此测试类需要在Java1.8下运行）
 
+		在本测试类的xml文件中，有两个类A和B，A依赖于B，B也依赖于A
+		同时有一个Advisor，里面装着A的前置增强类，那么在createBean方法中调用doCreateBean
+		会先创建A的实例，然后在doCreateBean中调用initializeBean，执行BeanPostProcessor的后置方法时
+		会调用DefaultAdvisorAutoProxyCreator#postProcessAfterInitialization方法，去创建代理对象
+
+
+	 */
 	@Test
 	public void testCircularReference() throws Exception {
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:circular-reference-with-proxy-bean.xml");
